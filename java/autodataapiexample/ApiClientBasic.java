617c8c33cb3cb4421b9e941dfce1f35ee9abed29
AutodataGroup <customer-integration@autodata-group.com>
2016-01-14 11:55:12 +0000

    Update ApiClientBasic.java

--- java/autodataapiexample/ApiClientBasic.java
+++ java/autodataapiexample/ApiClientBasic.java
@@ -1,5 +1,11 @@
+/*************************************************************
+Copyright (C) Autodata Limited 1974-2015.
+All Rights Reserved.
+NOTICE: Code example provide to developers wishing to integrate with the
+Autodata API. Only to be use under an NDA or valid contract with Autodata.
+Autodata are not liable for this code being re-used or modified.
+*/
 package autodataapiexample;
-
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
