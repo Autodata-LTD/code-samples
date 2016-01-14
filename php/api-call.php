5c835980b8dccb830bfbeecfd3461efd8ffbe46e
AutodataGroup <customer-integration@autodata-group.com>
2016-01-14 13:41:47 +0000

    Update api-call.php

--- php/api-call.php
+++ php/api-call.php
@@ -1,3 +1,10 @@
+/*************************************************************
+Copyright (C) Autodata Limited 1974-2015.
+All Rights Reserved.
+NOTICE: Code example provide to developers wishing to integrate with the
+Autodata API. Only to be use under an NDA or valid contract with Autodata.
+Autodata are not liable for this code being re-used or modified.
+*/
 <?php
 
 include_once 'Api.php';
